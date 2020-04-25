2b023a6df9ff2b9b118db757bf0aab829e0d66e0
Ryne <hansonryne@gmail.com>
2020-04-25 00:47:59 +0000

    Add entrypont

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Installing new gems if needed"
+bundle check || bundle install
+
+#echo "Copying node cache"
+#rsync -av /node_cache/. /app/
+
+echo "Creating database..."
+bundle exec rake db:create
+echo "Running database migrations..."
+bundle exec rake db:migrate
+
+#echo "Precompiling webpack assets"
+#bundle exec rake assets:precompile
+
+echo "Removing any old server pids"
+rm -f /app/tmp/pids/server.pid
+
+echo "Starting rails server"
+bundle exec rails s -b 0.0.0.0
+
